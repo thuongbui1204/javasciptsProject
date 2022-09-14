33bd55a8003789a318258572f8953a72558c92de
thuongbui1204 <110163365+thuongbui1204@users.noreply.github.com>
2022-09-14 12:35:04 +0700

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,8 @@
+"use strict";
+//1. Tạo Class User
+const User = function (Firstname, Lastname, Username, Password) {
+  this.firstName = Firstname;
+  this.lastName = Lastname;
+  this.userName = Username;
+  this.passWord = Password;
+};
